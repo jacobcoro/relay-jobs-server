36156a30a6d86b8a6230ae7431222d1a2d98f780
jacobcoro <xiaokourou@gmail.com>
2023-12-20 11:03:18 +0800

    remove dist

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexRouter = void 0;
-const express_1 = require("express");
-let indexRouter = (0, express_1.Router)();
-exports.indexRouter = indexRouter;
-indexRouter.get('*', (req, res) => {
-    res.send({ reply: 'pong' });
-});
